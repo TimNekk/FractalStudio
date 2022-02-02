f60f1ca8d82c61b73bdb347875f4072efd853635
Tim <herew26@gmail.com>
2022-02-02 19:28:45 +0300

    MainWindow.xaml.cs edited

--- FractalStudio/MainWindow.xaml.cs
+++ FractalStudio/MainWindow.xaml.cs
@@ -92,6 +92,7 @@ namespace FractalStudio
             {
                 _spacing = value;
                 SpacingSlider.Value = value;
+                FunctionComboBox.SelectedIndex = (int) value;
                 if (Fractal != null) Fractal.Spacing = value;
             }
         }
@@ -104,8 +105,49 @@ namespace FractalStudio
             set
             {
                 if (value < 0) return;
+                ScaleSlider.Value = value;
                 _scale = value;
                 if (Fractal != null) Fractal.Scale = value;
+            }
+        }
+        
+        private int _resolution;
+        
+        public int Resolution
+        {
+            get => _resolution;
+            set
+            {
+                if (value < 0) return;
+                ResolutionSlider.Value = value;
+                _resolution = value;
+                if (Fractal != null) Fractal.LengthRatio = value;
+            }
+        }
+        
+        private double _reRatio;
+        
+        public double ReRatio
+        {
+            get => _reRatio;
+            set
+            {
+                ReRatioSlider.Value = value;
+                _reRatio = value;
+                if (Fractal != null) Fractal.AngleRatioLeft = value;
+            }
+        }
+        
+        private double _imRatio;
+        
+        public double ImRatio
+        {
+            get => _imRatio;
+            set
+            {
+                ImRatioSlider.Value = value;
+                _imRatio = value;
+                if (Fractal != null) Fractal.AngleRatioRight = value;
             }
         }
         
@@ -116,7 +158,6 @@ namespace FractalStudio
             get => _x;
             set
             {
-                if (value < 0) return;
                 _x = value;
                 if (Fractal != null) Fractal.X = value;
             }
@@ -129,7 +170,6 @@ namespace FractalStudio
             get => _y;
             set
             {
-                if (value < 0) return;
                 _y = value;
                 if (Fractal != null) Fractal.Y = value;
             }
@@ -157,18 +197,21 @@ namespace FractalStudio
                 SetFrameGradient();
             }
         }
-        
+
         public MainWindow()
         {
             InitializeComponent();
-
+            
             MaxRecursion = 10;
             LengthRatio = 0.8;
-            Recursion = 2;
-            AngleRatioRight = 0.1;
-            AngleRatioLeft = 0.1;
+            Recursion = 5;
+            Scale = 1;
+            AngleRatioRight = 0.3;
+            AngleRatioLeft = 0.3;
             Spacing = 35;
             Gradient = new Gradient(Colors.Fuchsia, Colors.Cyan, 2);
+            Resolution = 100;
+            ReRatio = 0.1;
         }
 
         private void ControlPanelOnMouseDown(object sender, MouseButtonEventArgs e)
@@ -202,13 +245,17 @@ namespace FractalStudio
         private void PythagorasTreeButtonClick(object sender, RoutedEventArgs e)
         {
             MaxRecursion = 10;
+            X = 500;
+            Y = 500;
             Fractal = new PythagorasTree(FractalCanvas, Recursion, 500, 500, 1, LengthRatio, AngleRatioLeft, AngleRatioRight, Spacing, Gradient);
             Fractal.Draw();
         }
-        
+
         private void KochСurveButtonClick(object sender, RoutedEventArgs e)
         {
             MaxRecursion = 6;
+            X = 100;
+            Y = 500;
             Fractal = new KochСurve(FractalCanvas, Recursion, 100, 500, 1, LengthRatio, AngleRatioLeft, AngleRatioRight, Spacing, Gradient);
             Fractal.Draw();
         }
@@ -216,6 +263,8 @@ namespace FractalStudio
         private void SierpinskiCarpetButtonClick(object sender, RoutedEventArgs e)
         {
             MaxRecursion = 5;
+            X = 100;
+            Y = 100;
             Fractal = new SierpinskiCarpet(FractalCanvas, Recursion, 100, 100, 1, LengthRatio, AngleRatioLeft, AngleRatioRight, Spacing, Gradient);
             Fractal.Draw();
         }
@@ -223,14 +272,30 @@ namespace FractalStudio
         private void SierpinskiTriangleButtonClick(object sender, RoutedEventArgs e)
         {
             MaxRecursion = 7;
+            X = 400;
+            Y = 400;
             Fractal = new SierpinskiTriangle(FractalCanvas, Recursion, 400, 400, 1, LengthRatio, AngleRatioLeft, AngleRatioRight, Spacing, Gradient);
             Fractal.Draw();
         }
         
         private void CantorSetButtonClick(object sender, RoutedEventArgs e)
         {
+            Spacing = 35;
             MaxRecursion = 7;
+            X = 100;
+            Y = 100;
             Fractal = new CantorSet(FractalCanvas, Recursion, 100, 100, 1, LengthRatio, AngleRatioLeft, AngleRatioRight, Spacing, Gradient);
+            Fractal.Draw();
+        }
+        
+        private void JuliaSetButtonClick(object sender, RoutedEventArgs e)
+        {
+            MaxRecursion = 50;
+            X = 0;
+            Y = 0;
+            Scale = 1;
+            Spacing = 0;
+            Fractal = new JuliaSet(FractalCanvas, Recursion, 0, 0, 1, Resolution, AngleRatioLeft, AngleRatioRight, 0, Gradient, ImageControl);
             Fractal.Draw();
         }
 
@@ -293,6 +358,26 @@ namespace FractalStudio
         {
             Spacing = SpacingSlider.Value;
         }
+        
+        private void ScaleSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Scale = ScaleSlider.Value;
+        }        
+        
+        private void ResolutionSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Resolution = (int)ResolutionSlider.Value;
+        }       
+        
+        private void ReRatioSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ReRatio = ReRatioSlider.Value;
+        }
+        
+        private void ImRatioSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ImRatio = ImRatioSlider.Value;
+        }
 
         private void Zoom(object sender, MouseWheelEventArgs e)
         {
@@ -302,14 +387,17 @@ namespace FractalStudio
 
         private void PanMouseDown(object sender, MouseButtonEventArgs e)
         {
-            CanvasBorder.CaptureMouse();
+            // MessageBox.Show("Опустил");
             _previousMousePosition = e.GetPosition(CanvasBorder);
+            CanvasBorder.CaptureMouse();
         }
         
         private void PanMouseUp(object sender, MouseButtonEventArgs e)
         {
             CanvasBorder.ReleaseMouseCapture();
+            // MessageBox.Show("Поднял");
         }
+        
 
         private void PanMouseMove(object sender, MouseEventArgs e)
         {
@@ -324,15 +412,11 @@ namespace FractalStudio
 
         private void SaveButtonClick(object sender, RoutedEventArgs e)
         {
-            var rtb = new RenderTargetBitmap(
-                (int)FractalCanvas.RenderSize.Width,
-                (int)FractalCanvas.RenderSize.Height, 96d, 96d, PixelFormats.Default);
-            rtb.Render(FractalCanvas);
+            var rtb = new RenderTargetBitmap((int)FractalCanvas.RenderSize.Width, (int)FractalCanvas.RenderSize.Height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(JuliaSetButton.IsChecked is true ? ImageControl : FractalCanvas);
             
-            var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, (int) FractalCanvas.RenderSize.Width, (int) FractalCanvas.RenderSize.Height));
-
             BitmapEncoder pngEncoder = new PngBitmapEncoder();
-            pngEncoder.Frames.Add(BitmapFrame.Create(crop));
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
 
             var dialog = new SaveFileDialog
             {
@@ -340,12 +424,23 @@ namespace FractalStudio
                 InitialDirectory = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName
             };
             
-
             if (dialog.ShowDialog() == false)
                 return;
 
             using var fs = File.OpenWrite(dialog.FileName);
             pngEncoder.Save(fs);
+        }
+
+        private void FunctionComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Spacing = FunctionComboBox.SelectedIndex;
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
     }
 }
